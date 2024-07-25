3faa1ce0c6c5389d6fa1e7af8fcec58dd91a85fb
Varun Thakore <vrnthakore@gmail.com>
2024-07-25 23:54:06 +0530

    feat: implement Hasher for Blake2s256

--- merkle/src/hash.rs
+++ merkle/src/hash.rs
@@ -1,13 +1,37 @@
-use ark_ff::Field;
+use std::marker::PhantomData;
+use ark_ff::{BigInteger, PrimeField};
+use blake2::{Blake2s256, Digest};
 
-pub trait Hasher<F: Field> {
+pub trait Hasher<F: PrimeField> {
     type Output: Clone + Eq;
 
     // compress a list of internal nodes into a single internal node
-    #[allow(dead_code)]
     fn node(input: &[Self::Output]) -> Self::Output;
 
     // compress a list of leaves into a single leaf
-    #[allow(dead_code)]
     fn leaf(input: &[F]) -> Self::Output;
+}
+
+pub struct Blake2s256Hasher<F: PrimeField> {
+    _ph: PhantomData<F>,
+}
+
+impl<F: PrimeField> Hasher<F> for Blake2s256Hasher<F> {
+
+    type Output = [u8; 32];
+
+    fn leaf(input: &[F]) -> Self::Output {
+        let input_bytes: Vec<u8> = input.iter().flat_map(|f| f.into_bigint().to_bytes_be()).collect();
+        let mut hasher = Blake2s256::new();
+        hasher.update(input_bytes);
+        hasher.finalize().into()
+    }
+
+    fn node(input: &[Self::Output]) -> Self::Output {
+        let input_bytes: Vec<u8> = input.iter().flat_map(|&array| array).collect::<Vec<u8>>();
+        let mut hasher = Blake2s256::new();
+        hasher.update(input_bytes);
+        hasher.finalize().into()
+    }
+
 }
