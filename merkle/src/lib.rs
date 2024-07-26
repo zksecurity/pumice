45e4143f76cddd0875783900c69e6cd64bf93854
Varun Thakore <vrnthakore@gmail.com>
2024-07-26 23:51:23 +0530

    test: add tests for Keccak256

--- merkle/src/lib.rs
+++ merkle/src/lib.rs
@@ -228,81 +228,11 @@ mod tests {
     use hex_literal::hex;
 
     use crate::{
-        hash::{Blake2s256Hasher, Hasher},
+        hash::{Blake2s256Hasher, Hasher, Keccak256Hasher},
         MerkleTree,
     };
 
     use felt::Felt252;
-
-    fn test_compute_root<F, H>(input: &[H::Output], root_exp: H::Output)
-    where
-        F: PrimeField,
-        H: Hasher<F>,
-    {
-        let tree = MerkleTree::<F, H>::new(input);
-        assert_eq!(tree.root, root_exp);
-    }
-
-    #[test]
-    fn test_compute_root_with() {
-        // test vectors generated from https://github.com/starkware-libs/stone-prover/blob/main/src/starkware/commitment_scheme/merkle/merkle_test.cc
-
-        let input = [hex!(
-            "216acdc6a1fe9e6b89605b2eb3452c613b4ebc09af6c8477bf79d69fa9ec1125"
-        )];
-        let root_exp = hex!("216acdc6a1fe9e6b89605b2eb3452c613b4ebc09af6c8477bf79d69fa9ec1125");
-        test_compute_root::<Felt252, Blake2s256Hasher<Felt252>>(&input, root_exp);
-
-        let input = [
-            hex!("d7005ac2e5ece2a48746ae40264076edf63fc833532572d359a0c47cbc42c482"),
-            hex!("6dc48ad654bc4a3e3c8f3270a987b2782af1707e78e1512018f16fdee124bdbd"),
-        ];
-        let root_exp = hex!("b823ac891cee85512521528e1d61cdcad829080392b63f393e46adc213862af9");
-        test_compute_root::<Felt252, Blake2s256Hasher<Felt252>>(&input, root_exp);
-
-        let input = [
-            hex!("1363b8220c90c05d93c279ab287fadfe2e223543ca46dee65b32ecd7d7bc891c"),
-            hex!("12ee05a2b647db0371c79c13842574c13ccc3610faa888aa4ec4764407fe84ed"),
-            hex!("aa7ea89c5a679c4aa2c7f85d58988bc42eb97828f5bf18a8fca88f52fe3cf933"),
-            hex!("1c16cd6af66980b3f38f66a213a82b0c5d3480296d6315d12ea3862e72e29a8c"),
-        ];
-        let root_exp = hex!("57989eb39d929071d2f3a4400067d00e4d77671d9ed682a8cdd2d4e11d3f5aea");
-        test_compute_root::<Felt252, Blake2s256Hasher<Felt252>>(&input, root_exp);
-
-        let input = [
-            hex!("72413d150744cf57eb6588853f44855b03b360b232a2109b83bed983369d1412"),
-            hex!("914d9ac0dbc6d97e427340798b5ac8b5c1f4c5cd7be177d55463665965e334c4"),
-            hex!("969477390c57491d9237cf4fb1cecc17584c0a7b9b0a3db6255899779ab8f52d"),
-            hex!("59e28ede24abe8ee0a8319ddd90beb9725edaa107531f1497574fe745d153d59"),
-            hex!("c28ed6b97ca7ba227402b12d3c4fe73d6ab45aad72e81c20c7fd32816db1398c"),
-            hex!("7b78d48039e32a055df717b065e478207c5693f37d9ce6229c55e13f8d71b397"),
-            hex!("dd4b9612ef3cb88534a8602320145118bd22781d29c396b8686f4871f3727764"),
-            hex!("f5f865937bd28b5c996d94627226888dbad14a5b024f5962fd45c5f62315fb63"),
-        ];
-        let root_exp = hex!("3f8893c325c0ddbd7b8d6c7340b30861bbb79ad64562124e25aa47499c582de3");
-        test_compute_root::<Felt252, Blake2s256Hasher<Felt252>>(&input, root_exp);
-
-        let input = [
-            hex!("b834a8385e53f736e02e308351cde4a3e9a70c83446487cb3397ca7cfc868fd2"),
-            hex!("5761708eb0ac5f10ad04d71415453f53bfeca9f8454e0a85037ced8e09ad536e"),
-            hex!("04203a2181b8948292ffbaf7dfbd0e70d9b71ecab2a6075a39828fde26a54415"),
-            hex!("bd03e9abeb519b817c2d96f6a227755e6807656f24075e5933895f38b81ca840"),
-            hex!("9c0a39bd966e29420d9e4e9718d0238fc8935649202ff7056e40aaa5bcea62ef"),
-            hex!("3c74f69c462b05212087673174f4f24a9d5f65e55868a1d593467abb3905d5bb"),
-            hex!("e2cea6e8e32426b23b38f766720dc7ebf72865bac608c8c1030ef806b84ebb97"),
-            hex!("e081a1e8983546099bce2967607f0c25d43bd01ae564c4212bb4931e38b6ffad"),
-            hex!("d3dfec774ab0b0433a3a32766d50c11fdcfdd3f97785cce28d3b23ed963694f9"),
-            hex!("0d835a0558af1b4c8664228ebf7ea6a6440cd6fd2387573e0c5bee2b03d8c506"),
-            hex!("d5559fbd925ce10cf9e7b734d5c0b25f898d56d3dac7c3253130781460d35fb4"),
-            hex!("bc9a983f00efa883f39a1e168a18d3f8d47841acc34640d80fafda40968cf2ad"),
-            hex!("d035d3b16da2e6a1f355d6d650f9c891e3b030f0612521e5431f42a162e92e41"),
-            hex!("02ec5ddb356c692553d911625def6369aa8c4ad46cea51c7b6b5f7d2d17d3244"),
-            hex!("04f20878faaac6e3eb11b2d7f7490661138b355b4fcd7a1436705f42a68de61c"),
-            hex!("3a579a12f0250c86182920fad2048ab0d9195b6e3461160db36b4a4eb63399f6"),
-        ];
-        let root_exp = hex!("eeae978671e1365726c7247cf945725bbaebe267260530a871153b39064ab342");
-        test_compute_root::<Felt252, Blake2s256Hasher<Felt252>>(&input, root_exp);
-    }
 
     fn test_verify_true<F, H>(
         input: &[H::Output],
@@ -321,6 +251,7 @@ mod tests {
     fn test_verify_true_with() {
         // test vectors generated from https://github.com/starkware-libs/stone-prover/blob/main/src/starkware/commitment_scheme/merkle/merkle_test.cc
 
+        // tests using Blake2s256
         let input = [hex!(
             "216acdc6a1fe9e6b89605b2eb3452c613b4ebc09af6c8477bf79d69fa9ec1125"
         )];
@@ -509,6 +440,113 @@ mod tests {
             ),
         ];
         test_verify_true::<Felt252, Blake2s256Hasher<Felt252>>(&input, root_exp, &to_verify);
+
+        // tests using Keccak256
+        let input = [hex!(
+            "ed5f0c2a7fdf07022be01ec165a50601807910d9dcc0ce3def6d291e9675e127"
+        )];
+        let root_exp = hex!("ed5f0c2a7fdf07022be01ec165a50601807910d9dcc0ce3def6d291e9675e127");
+        let to_verify = [(
+            0,
+            hex!("ed5f0c2a7fdf07022be01ec165a50601807910d9dcc0ce3def6d291e9675e127"),
+        )];
+        test_verify_true::<Felt252, Keccak256Hasher<Felt252>>(&input, root_exp, &to_verify);
+
+        let input = [
+            hex!("8b471e237284b2a8d0845469431ecc163264d4924c984987a4dac5c40e2c34f2"),
+            hex!("a1482873bccd9dd66ec6ef8c8a092c7f70839af97b22936ffa5606f1cd28dcea"),
+        ];
+        let root_exp = hex!("f272f3ba749e68b9ccd51f322c6ae6cac8734967ecc31c1058b8f9a8c99fb083");
+        let to_verify = [(
+            1,
+            hex!("a1482873bccd9dd66ec6ef8c8a092c7f70839af97b22936ffa5606f1cd28dcea"),
+        )];
+        test_verify_true::<Felt252, Keccak256Hasher<Felt252>>(&input, root_exp, &to_verify);
+
+        let input = [
+            hex!("b77b78e96b3f1de1a11ad49cae9804d8fd754123c916f25c638dd84d7ef8687d"),
+            hex!("15014fef710a715ea5a879a328912d8e1feefbc23137e8bf223139285fd9203e"),
+            hex!("2483654ae6756dc99e99173802f5737cf44d23ffa8b8fae732160c14db636793"),
+            hex!("c0e9d48d53c45ebfa3cd5ba2492638322463df296bab03b6492f4f11536e7e0e"),
+        ];
+        let root_exp = hex!("24562159dd8260d379ae390b80cb8266da3f4469bc6643db3b5c3f760c126b83");
+        let to_verify = [
+            (
+                1,
+                hex!("15014fef710a715ea5a879a328912d8e1feefbc23137e8bf223139285fd9203e"),
+            ),
+            (
+                3,
+                hex!("c0e9d48d53c45ebfa3cd5ba2492638322463df296bab03b6492f4f11536e7e0e"),
+            ),
+        ];
+        test_verify_true::<Felt252, Keccak256Hasher<Felt252>>(&input, root_exp, &to_verify);
+
+        let input = [
+            hex!("66661b6925e2abacbb63f975277eb908fa7d3f2c4de8d2cfe9788fd0e2af3234"),
+            hex!("fdc012059327bae5b7796e255b251a5eefcc4310f8438bb738c12964f0630da1"),
+            hex!("abe2a0f9213f7033dccb60a9742f9edcbbb7e952ca4eaf2736735155d66a9be9"),
+            hex!("6c63d57004dd9fb1f6a65d111e2230cca32f9aff392d0b84e6ba8c47fe093ae0"),
+            hex!("1e0841b00ea5648b2996203d58818b39e7c951c7f12de730aecaa69670e324f6"),
+            hex!("3213a156ac8029f8355208b9e6afd496775a494bee3bf4a2bbdaade80da9cc93"),
+            hex!("8e62dab2bb313c8e2ed86d601e47e3307e919fac5042be9faa53ba83902dd0a8"),
+            hex!("2fe79a0be4d26e69164bdd96404780e8e504c5e96eb975975236579c8b569a95"),
+        ];
+        let root_exp = hex!("50a1b9b02edd048756030d39f12e795ab7885565964dde0de6e5de1655f0d793");
+        let to_verify = [
+            (
+                0,
+                hex!("66661b6925e2abacbb63f975277eb908fa7d3f2c4de8d2cfe9788fd0e2af3234"),
+            ),
+            (
+                3,
+                hex!("6c63d57004dd9fb1f6a65d111e2230cca32f9aff392d0b84e6ba8c47fe093ae0"),
+            ),
+            (
+                4,
+                hex!("1e0841b00ea5648b2996203d58818b39e7c951c7f12de730aecaa69670e324f6"),
+            ),
+            (
+                7,
+                hex!("2fe79a0be4d26e69164bdd96404780e8e504c5e96eb975975236579c8b569a95"),
+            ),
+        ];
+        test_verify_true::<Felt252, Keccak256Hasher<Felt252>>(&input, root_exp, &to_verify);
+
+        let input = [
+            hex!("1edc3c8392034de0f516534ed7f5441f971d6fa78e1458107939398aaf35162c"),
+            hex!("e6d004f42a562534a47d304aaf52425773b70d11c0be3bd59112a0ccf1f0ab2b"),
+            hex!("614a71ae7c9c127e71884f6bc64454842977b58be4dfcd3a2db8763bba5e8ae5"),
+            hex!("564b9ffa2508e2d59c2d5f43c4ca96b7646f3b2363814dac042a89b48af183a3"),
+            hex!("e4ef3f3ea1ae8c961aa6ef36cfdd91831aaa2c92d832aee4687a46144ebbef83"),
+            hex!("5462d01e69b3006ea33734a6eea7dbf05d983a46fed38522ac2fae8373b0c715"),
+            hex!("3136305dd4779e01c586fb66b0400d44606dfd3a38c51eda04dc3c059b3c3215"),
+            hex!("0085f15f8357aecc4ed40fa7de7a8c44e4800ef5f556d73d6d9e8e77e99eefce"),
+            hex!("c11b6869d1674f3342a366f2fc728bed396ab858a36d0d125dea64fb2495e7ac"),
+            hex!("ba350a7e4e86f425f9624be90cb4a82c59263304aa02acd8fcb6e246cdc9b12c"),
+            hex!("19350ec1a9d255a21556d63d9e35bfb0839ceab89d4d221e92d6cf68ba02bbd5"),
+            hex!("9c3982feabacd25de79b03852235fb8c757c2f41912aeda46a5f84b534e2ad40"),
+            hex!("87204b53f02a216e574b13e78a7ab11c15181ccbab05694039cfae3333e308a0"),
+            hex!("81ac8a8b30bf7170f11d23847729f335bfeaa4107a7acd68a87eb43a097bfc36"),
+            hex!("0678011aba74ebbdc6d79093b39aa4b6d4c88eb96043a95537633f361e1dc4ed"),
+            hex!("8a45012a030697b1c0db1504c68ee0fca1ab0ac8e3ad80c032b26f7b38ea97b4"),
+        ];
+        let root_exp = hex!("fba6d37293ad9845ff546e9615853594c47237ee666cf5267788e14e0032f3de");
+        let to_verify = [
+            (
+                4,
+                hex!("e4ef3f3ea1ae8c961aa6ef36cfdd91831aaa2c92d832aee4687a46144ebbef83"),
+            ),
+            (
+                5,
+                hex!("5462d01e69b3006ea33734a6eea7dbf05d983a46fed38522ac2fae8373b0c715"),
+            ),
+            (
+                13,
+                hex!("81ac8a8b30bf7170f11d23847729f335bfeaa4107a7acd68a87eb43a097bfc36"),
+            ),
+        ];
+        test_verify_true::<Felt252, Keccak256Hasher<Felt252>>(&input, root_exp, &to_verify);
     }
 
     fn test_verify_false<F, H>(
@@ -526,6 +564,7 @@ mod tests {
 
     #[test]
     fn test_verify_false_with() {
+        // tests using Blake2s256
         let input = [hex!(
             "216acdc6a1fe9e6b89605b2eb3452c613b4ebc09af6c8477bf79d69fa9ec1125"
         )];
@@ -714,5 +753,112 @@ mod tests {
             ),
         ];
         test_verify_false::<Felt252, Blake2s256Hasher<Felt252>>(&input, root_exp, &to_verify);
+
+        // tests using Keccak256
+        let input = [hex!(
+            "ed5f0c2a7fdf07022be01ec165a50601807910d9dcc0ce3def6d291e9675e127"
+        )];
+        let root_exp = hex!("ed5f0c2a7fdf07022be01ec165a50601807910d9dcc0ce3def6d291e9675e127");
+        let to_verify = [(
+            0,
+            hex!("8b471e237284b2a8d0845469431ecc163264d4924c984987a4dac5c40e2c34f2"),
+        )];
+        test_verify_false::<Felt252, Keccak256Hasher<Felt252>>(&input, root_exp, &to_verify);
+
+        let input = [
+            hex!("8b471e237284b2a8d0845469431ecc163264d4924c984987a4dac5c40e2c34f2"),
+            hex!("a1482873bccd9dd66ec6ef8c8a092c7f70839af97b22936ffa5606f1cd28dcea"),
+        ];
+        let root_exp = hex!("f272f3ba749e68b9ccd51f322c6ae6cac8734967ecc31c1058b8f9a8c99fb083");
+        let to_verify = [(
+            1,
+            hex!("8b471e237284b2a8d0845469431ecc163264d4924c984987a4dac5c40e2c34f2"),
+        )];
+        test_verify_false::<Felt252, Keccak256Hasher<Felt252>>(&input, root_exp, &to_verify);
+
+        let input = [
+            hex!("b77b78e96b3f1de1a11ad49cae9804d8fd754123c916f25c638dd84d7ef8687d"),
+            hex!("15014fef710a715ea5a879a328912d8e1feefbc23137e8bf223139285fd9203e"),
+            hex!("2483654ae6756dc99e99173802f5737cf44d23ffa8b8fae732160c14db636793"),
+            hex!("c0e9d48d53c45ebfa3cd5ba2492638322463df296bab03b6492f4f11536e7e0e"),
+        ];
+        let root_exp = hex!("24562159dd8260d379ae390b80cb8266da3f4469bc6643db3b5c3f760c126b83");
+        let to_verify = [
+            (
+                1,
+                hex!("15014fef710a715ea5a879a328912d8e1feefbc23137e8bf223139285fd9203e"),
+            ),
+            (
+                3,
+                hex!("2483654ae6756dc99e99173802f5737cf44d23ffa8b8fae732160c14db636793"),
+            ),
+        ];
+        test_verify_false::<Felt252, Keccak256Hasher<Felt252>>(&input, root_exp, &to_verify);
+
+        let input = [
+            hex!("66661b6925e2abacbb63f975277eb908fa7d3f2c4de8d2cfe9788fd0e2af3234"),
+            hex!("fdc012059327bae5b7796e255b251a5eefcc4310f8438bb738c12964f0630da1"),
+            hex!("abe2a0f9213f7033dccb60a9742f9edcbbb7e952ca4eaf2736735155d66a9be9"),
+            hex!("6c63d57004dd9fb1f6a65d111e2230cca32f9aff392d0b84e6ba8c47fe093ae0"),
+            hex!("1e0841b00ea5648b2996203d58818b39e7c951c7f12de730aecaa69670e324f6"),
+            hex!("3213a156ac8029f8355208b9e6afd496775a494bee3bf4a2bbdaade80da9cc93"),
+            hex!("8e62dab2bb313c8e2ed86d601e47e3307e919fac5042be9faa53ba83902dd0a8"),
+            hex!("2fe79a0be4d26e69164bdd96404780e8e504c5e96eb975975236579c8b569a95"),
+        ];
+        let root_exp = hex!("50a1b9b02edd048756030d39f12e795ab7885565964dde0de6e5de1655f0d793");
+        let to_verify = [
+            (
+                0,
+                hex!("fdc012059327bae5b7796e255b251a5eefcc4310f8438bb738c12964f0630da1"),
+            ),
+            (
+                3,
+                hex!("abe2a0f9213f7033dccb60a9742f9edcbbb7e952ca4eaf2736735155d66a9be9"),
+            ),
+            (
+                4,
+                hex!("1e0841b00ea5648b2996203d58818b39e7c951c7f12de730aecaa69670e324f6"),
+            ),
+            (
+                7,
+                hex!("2fe79a0be4d26e69164bdd96404780e8e504c5e96eb975975236579c8b569a95"),
+            ),
+        ];
+        test_verify_false::<Felt252, Keccak256Hasher<Felt252>>(&input, root_exp, &to_verify);
+
+        let input = [
+            hex!("1edc3c8392034de0f516534ed7f5441f971d6fa78e1458107939398aaf35162c"),
+            hex!("e6d004f42a562534a47d304aaf52425773b70d11c0be3bd59112a0ccf1f0ab2b"),
+            hex!("614a71ae7c9c127e71884f6bc64454842977b58be4dfcd3a2db8763bba5e8ae5"),
+            hex!("564b9ffa2508e2d59c2d5f43c4ca96b7646f3b2363814dac042a89b48af183a3"),
+            hex!("e4ef3f3ea1ae8c961aa6ef36cfdd91831aaa2c92d832aee4687a46144ebbef83"),
+            hex!("5462d01e69b3006ea33734a6eea7dbf05d983a46fed38522ac2fae8373b0c715"),
+            hex!("3136305dd4779e01c586fb66b0400d44606dfd3a38c51eda04dc3c059b3c3215"),
+            hex!("0085f15f8357aecc4ed40fa7de7a8c44e4800ef5f556d73d6d9e8e77e99eefce"),
+            hex!("c11b6869d1674f3342a366f2fc728bed396ab858a36d0d125dea64fb2495e7ac"),
+            hex!("ba350a7e4e86f425f9624be90cb4a82c59263304aa02acd8fcb6e246cdc9b12c"),
+            hex!("19350ec1a9d255a21556d63d9e35bfb0839ceab89d4d221e92d6cf68ba02bbd5"),
+            hex!("9c3982feabacd25de79b03852235fb8c757c2f41912aeda46a5f84b534e2ad40"),
+            hex!("87204b53f02a216e574b13e78a7ab11c15181ccbab05694039cfae3333e308a0"),
+            hex!("81ac8a8b30bf7170f11d23847729f335bfeaa4107a7acd68a87eb43a097bfc36"),
+            hex!("0678011aba74ebbdc6d79093b39aa4b6d4c88eb96043a95537633f361e1dc4ed"),
+            hex!("8a45012a030697b1c0db1504c68ee0fca1ab0ac8e3ad80c032b26f7b38ea97b4"),
+        ];
+        let root_exp = hex!("fba6d37293ad9845ff546e9615853594c47237ee666cf5267788e14e0032f3de");
+        let to_verify = [
+            (
+                4,
+                hex!("e4ef3f3ea1ae8c961aa6ef36cfdd91831aaa2c92d832aee4687a46144ebbef83"),
+            ),
+            (
+                5,
+                hex!("5462d01e69b3006ea33734a6eea7dbf05d983a46fed38522ac2fae8373b0c715"),
+            ),
+            (
+                13,
+                hex!("614a71ae7c9c127e71884f6bc64454842977b58be4dfcd3a2db8763bba5e8ae5"),
+            ),
+        ];
+        test_verify_false::<Felt252, Keccak256Hasher<Felt252>>(&input, root_exp, &to_verify);
     }
 }
