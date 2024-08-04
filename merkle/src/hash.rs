f452bb6b45a53a741e0cb58ab0ec055d6897fc50
Varun Thakore <vrnthakore@gmail.com>
2024-08-04 23:43:58 +0530

    fix: update hasher trait impl

--- merkle/src/hash.rs
+++ merkle/src/hash.rs
@@ -23,19 +23,16 @@ impl<F: PrimeField> Hasher<F> for Blake2s256Hasher<F> {
     type Output = [u8; 32];
 
     fn leaf(input: &[F]) -> Self::Output {
-        let input_bytes: Vec<u8> = input
-            .iter()
-            .flat_map(|f| f.into_bigint().to_bytes_be())
-            .collect();
         let mut hasher = Blake2s256::new();
-        hasher.update(input_bytes);
+        input
+            .iter()
+            .for_each(|f| hasher.update(f.into_bigint().to_bytes_be()));
         hasher.finalize().into()
     }
 
     fn node(input: &[Self::Output]) -> Self::Output {
-        let input_bytes: Vec<u8> = input.iter().flat_map(|&array| array).collect::<Vec<u8>>();
         let mut hasher = Blake2s256::new();
-        hasher.update(input_bytes);
+        input.iter().for_each(|f| hasher.update(f));
         hasher.finalize().into()
     }
 }
@@ -48,19 +45,16 @@ impl<F: PrimeField> Hasher<F> for Keccak256Hasher<F> {
     type Output = [u8; 32];
 
     fn leaf(input: &[F]) -> Self::Output {
-        let input_bytes: Vec<u8> = input
-            .iter()
-            .flat_map(|f| f.into_bigint().to_bytes_be())
-            .collect();
         let mut hasher = Keccak256::new();
-        hasher.update(input_bytes);
+        input
+            .iter()
+            .for_each(|f| hasher.update(f.into_bigint().to_bytes_be()));
         hasher.finalize().into()
     }
 
     fn node(input: &[Self::Output]) -> Self::Output {
-        let input_bytes: Vec<u8> = input.iter().flat_map(|&array| array).collect::<Vec<u8>>();
         let mut hasher = Keccak256::new();
-        hasher.update(input_bytes);
+        input.iter().for_each(|f| hasher.update(f));
         hasher.finalize().into()
     }
 }
@@ -73,6 +67,7 @@ impl Hasher<Felt252> for Poseidon3<Felt252> {
     }
 
     fn node(input: &[Self::Output]) -> Self::Output {
-        Poseidon3::hash(input)
+        assert_eq!(input.len(), 2);
+        Poseidon3::pair(input[0], input[1])
     }
 }
