3dc09ecb7967ea7dd30470d315c37ff345f8f742
Varun Thakore <vrnthakore@gmail.com>
2024-09-23 01:38:23 +0530

    feat: implment FRI Prover

--- fri/src/parameters.rs
+++ fri/src/parameters.rs
@@ -7,6 +7,7 @@ use serde_json::Value as JsonValue;
 
 //#[derive(Deserialize)]
 #[allow(dead_code)]
+#[derive(Clone)]
 pub struct FriParameters<F: FftField, E: EvaluationDomain<F>> {
     pub ph: PhantomData<F>,
     /*
@@ -85,18 +86,4 @@ impl<F: FftField, E: EvaluationDomain<F>> FriParameters<F, E> {
             proof_of_work_bits: json["proof_of_work_bits"].as_u64().unwrap() as usize,
         }
     }
-}
-
-#[allow(dead_code)]
-pub struct FriProverConfig {
-    pub max_non_chunked_layer_size: u64,
-    pub n_chunks_between_layers: usize,
-    pub log_n_max_in_memory_fri_layer_elements: usize,
-}
-
-#[allow(dead_code)]
-impl FriProverConfig {
-    pub const DEFAULT_MAX_NON_CHUNKED_LAYER_SIZE: u64 = 32768;
-    pub const DEFAULT_NUMBER_OF_CHUNKS_BETWEEN_LAYERS: usize = 32;
-    pub const ALL_IN_MEMORY_LAYERS: usize = 63;
 }
